badca7eeba8dd8922df165d6aac77712dc8b1d77
anhnn12015 <anhnn.ba12-015@st.usth.edu.vn>
2024-09-14 14:21:38 +0700

    Practical 1-8

---
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherFragment.java
@@ -0,0 +1,77 @@
+package vn.edu.usth.weather;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Objects;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link WeatherFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class WeatherFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+
+    // TODO: Rename and change types of parameters
+    private String city_name;
+    private int weather_icon;
+
+    public WeatherFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+
+     * @return A new instance of fragment WeatherFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static WeatherFragment newInstance(String cityName, int weather_icon) {
+        WeatherFragment fragment = new WeatherFragment();
+        Bundle args = new Bundle();
+        args.putString("city", cityName);
+        args.putInt("weather_icon", weather_icon);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            city_name = getArguments().getString("city");
+            weather_icon = getArguments().getInt("weather_icon");
+        }
+    }
+
+    @SuppressLint({"MissingInflatedId", "LocalSuppress", "DiscouragedApi"})
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_weather, container, false);
+
+        TextView cityTextView = view.findViewById(R.id.city_name);
+        ImageView weatherImageView = view.findViewById(R.id.weather_icon);
+
+        cityTextView.setText(city_name);
+        weatherImageView.setImageResource(weather_icon);
+
+        return view;
+
+    }
+}
